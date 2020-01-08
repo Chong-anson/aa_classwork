ff1157d67ab43ca58de23d542c3c6c01e8cd735a
anson <ansonchongwus44@gmail.com>
2020-01-08 09:21:29 -0800

    Add feature to update employee list automatically

--- W4D2/skeleton/employee.rb
+++ W4D2/skeleton/employee.rb
@@ -1,14 +1,18 @@
 class Employee 
   attr_reader :salary
-  def initialize(name, title, salary, boss)
+  def initialize(name, title, salary, boss = nil)
     @name = name 
     @title = title 
     @salary = salary
-    @boss = boss
+    boss.employees << self if boss.is_a?(Manager)
     @bonus = 0
   end 
   
   def bonus(multiplier)
     @bonus = @salary * multiplier
+  end 
+
+  def inspect 
+    @name 
   end 
 end     
--- W4D2/skeleton/manager.rb
+++ W4D2/skeleton/manager.rb
@@ -28,5 +28,4 @@ darren = Manager.new("Darren", "TA Manager", 78000, ned)
 shawna = Employee.new("Shawna", "TA", 12000, darren)
 david = Employee.new("David", "TA", 10000, darren)
 
-ned.employees << darren 
-darren.employees += [shawna, david]
+
